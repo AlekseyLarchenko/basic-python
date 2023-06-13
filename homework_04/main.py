9e4e027e1f8879ab309f614b4d3c0a75dd16de42
aleksey <larchenko.aleksey@gmail.com>
2023-06-13 16:04:29 +0300

    homework_04 all

--- homework_04/main.py
+++ homework_04/main.py
@@ -12,14 +12,49 @@
   (используйте полученные из запроса данные, передайте их в функцию для добавления в БД)
 - закрытие соединения с БД
 """
+import asyncio
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from jsonplaceholder_requests import get_user_data, get_posts_data
+from models import Base, User, Post
+
+PG_CONN_URI = "postgresql+asyncpg://username:passwd@0.0.0.0:5433/blog"
+
+Engine = create_engine(PG_CONN_URI)
+Session = sessionmaker(
+    Engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+async def add_users_data():
+    session = Session()
+    users_data = await get_user_data()
+    users = [User(name=user['name'], username=user['username'], email=user['email']) for user in users_data]
+    session.add_all(users)
+    session.commit()
+    session.close()
+
+
+async def add_posts_data():
+    session = Session()
+    posts_data = await get_posts_data()
+    posts = [Post(user_id=post['userId'], title=post['title'], body=post['body']) for post in posts_data]
+    session.add_all(posts)
+    session.commit()
+    session.close()
 
 
 async def async_main():
-    pass
+    Base.metadata.create_all(Engine)
+    await asyncio.gather(add_users_data(), add_posts_data())
+    Engine.dispose()
 
 
 def main():
-    pass
+    asyncio.run(async_main())
 
 
 if __name__ == "__main__":
