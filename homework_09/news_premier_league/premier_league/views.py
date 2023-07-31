48e2afd48b352fd57725196f2930b9072f6a5de0
aleksey <larchenko.aleksey@gmail.com>
2023-07-31 18:57:45 +0300

    homework_09 copy hw8 and tests.py

---
+++ homework_09/news_premier_league/premier_league/views.py
@@ -0,0 +1,42 @@
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Stadium, Team, News
+
+
+class StadiumListView(ListView):
+    model = Stadium
+    template_name = 'premier_league/stadium_list.html'
+    context_object_name = 'stadiums'
+
+
+class StadiumDetailView(DetailView):
+    model = Stadium
+    template_name = 'premier_league/stadium_detail.html'
+    context_object_name = 'stadium'
+
+
+class TeamListView(ListView):
+    model = Team
+    template_name = 'premier_league/team_list.html'
+    context_object_name = 'teams'
+
+
+class TeamDetailView(DetailView):
+    model = Team
+    template_name = 'premier_league/team_detail.html'
+    context_object_name = 'team'
+
+
+class NewsListView(ListView):
+    model = News
+    template_name = 'premier_league/news_list.html'
+    context_object_name = 'news_list'
+
+
+class NewsDetailView(DetailView):
+    model = News
+    template_name = 'premier_league/news_detail.html'
+    context_object_name = 'news'
+
+
+class IndexView(TemplateView):
+    template_name = "premier_league/index.html"
